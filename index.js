a6008703568bb147eefa1a5513145d64da3e0daf
Faruqoloyede <oloyedefaruq2@gmail.com>
2024-08-13 07:44:30 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var id = 5;
+var company = "faruq company";
+var ispublished = true;
+var x = 'hello';
+var ids = [1, 2, 3, 4, 5];
+var arr = [1, true, 'hello '];
+console.log(ids);
---
+++ index.ts
@@ -0,0 +1,9 @@
+let id: number = 5
+let company: string = "faruq company"
+let ispublished: boolean = true
+let x: any = 'hello'
+
+let ids: number[] = [1, 2, 3, 4, 5]
+let arr: any[] = [1, true, 'hello ']
+
+console.log(ids);
